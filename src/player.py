a8a7a3f07b04de56ea741bb9e36875c5f7cabd75
Afia-Lydia <you@example.com>
2020-07-08 20:17:31 -0400

    created classes for rooms and players, started working on the game

--- src/player.py
+++ src/player.py
@@ -1,2 +1,20 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player: 
+    """ Player class holds information about the players and what room that they are currently in """
+
+    def __init__(self, name, current_room, items=[]):
+        self.name = name
+        self.current_room = current_room
+        self.items = items
+
+        # print('player.__init__')
+    
+    def __str__(self):
+        return f"{self.name} is currently playing the game."
+
+
+# test = Player("Carlyn", "backyard")
+
+# print(test)
